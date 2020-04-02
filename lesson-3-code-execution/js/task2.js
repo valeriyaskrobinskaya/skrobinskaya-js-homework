ac52da8a86b20a9d786c9922a560b6cbe7c2d038
unknown <you@example.com>
2020-04-02 14:40:12 +0300

    Finished task2

---
+++ lesson-3-code-execution/js/task2.js
@@ -0,0 +1,16 @@
+var image = {
+    width: 100,
+    height: 800,
+    title: 'Cool image'
+};
+
+function multiplyNumeric( obj ) {
+    for (var key in obj) {
+        if ( !isNaN(obj[key])){
+            obj[key] += obj[key] ;
+        };
+    };
+    return obj;
+};
+
+console.log(multiplyNumeric(image) );
