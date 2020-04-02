074abaedc59c5a3b617687942f50d1c312820538
unknown <you@example.com>
2020-04-02 14:46:41 +0300

    Finished task3

---
+++ lesson-3-code-execution/js/task3.js
@@ -0,0 +1,22 @@
+var arr = [];
+
+var check = true;
+var sum = 0;
+
+while(check){
+    var digit = prompt('Введите число', 0);
+    if(digit === '') {
+        check = false;
+    } else if(digit === null || isNaN(digit)){
+        check = false;
+    } else {
+        arr.push(Number(digit));
+    }
+
+}
+
+for(var i = 0; i < arr.length; i++){
+    sum += arr[i];
+}
+
+alert(sum);
