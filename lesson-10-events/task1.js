555d06755c53e45583004b219abe77b0a857e128
Valeriya Skrobinskaya <valeriya.skrobinskaya@gmail.com>
2020-04-30 12:03:12 +0300

    Add form elements array

---
+++ lesson-10-events/task1.js
@@ -0,0 +1,14 @@
+var array = [
+    {p: "Для внесения вашего сайта в каталог, заполните форму:", name:"form"},
+    {label:"Разработчики:",type:"text",name:"author"},
+    {label:"Название сайта:",type:"text",name:"title"},
+    {label:"URL сайта:",type:"text",name:"site"},
+    {label:"Дата запуска сайта:",type:"text",name:"startdate"},
+    {label:"Посетителей в сутки:",type:"text",name:"persons"},
+    {label:"E-mail для связи:",type:"email",name:"title"},
+    {label:"Рубрика каталога:",type:"select",name:"name",options:[{value:"1",text:"здоровье"},{value:"2",text:"уют"}, {value:"3",text:"бытовая техника"}]},
+    {label:"Размещение:",type:"radio",name:"public",options:[{value:"1",text:"бесплатное "},{value:"2",text:"платное"},{value:"3",text:"VIP"}]},
+    {label:"Разрешить отзывы",type:"checkbox",name:"comments",checked:"true"},
+    {label:"Описание сайта:",type:"textarea",name:"article"},
+    {value:"Опубликовать",type:"submit"}
+];
