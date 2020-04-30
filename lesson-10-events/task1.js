325e304e531253a5cbe132418f7552de934f2c28
Valeriya Skrobinskaya <valeriya.skrobinskaya@gmail.com>
2020-04-30 12:43:29 +0300

    Add changes

--- lesson-10-events/task1.js
+++ lesson-10-events/task1.js
@@ -20,20 +20,20 @@ function formAdd(arr,form) {
 
     function formAddInput(element, index, arr) {
         if (element.name === "form") {
-            var NewTagElement = document.createElement('p');
+            var NewTagElement = document.createElement("p");
             var NewTextElement = document.createTextNode(element.p);
             NewTagElement.appendChild(NewTextElement);
             form.appendChild(NewTagElement);
         }
 
         if (element.type === "tel" || element.type ==="email" ||  element.type === "text") {
-            // var NewTagElement = document.createElement('p');
-            var NewLabelElement=document.createElement('label');
-            var NewInputElement=document.createElement('input');
-            var NewTextElement=document.createTextNode(element.label);
+            var NewTagElement = document.createElement("p");
+            var NewLabelElement = document.createElement("label");
+            var NewInputElement = document.createElement("input");
+            var NewTextElement = document.createTextNode(element.label);
 
-            NewInputElement.type=element.type;
-            NewInputElement.name=element.name;
+            NewInputElement.type = element.type;
+            NewInputElement.name = element.name;
             NewLabelElement.appendChild(NewTextElement);
             NewLabelElement.appendChild(NewInputElement);
             NewTagElement.appendChild(NewLabelElement);
@@ -42,73 +42,74 @@ function formAdd(arr,form) {
 
         if(element.type ==="select") {
 
-            var NewTagElement = document.createElement('p');
-            var NewLabelElement = document.createElement('label');
+            var NewTagElement = document.createElement("p");
+            var NewLabelElement = document.createElement("label");
             var NewTextElement = document.createTextNode(element.label);
-            var NewSelectElement = document.createElement('select');
+            var NewSelectElement = document.createElement("select");
             NewSelectElement.name = element.name;
             NewSelectElement.classList.add(element.class);
 
             NewLabelElement.appendChild(NewTextElement);
 
             for (var i = 0; i < element.options.length; i++) {
-                var NewOptionElement = document.createElement('option');
+                var NewOptionElement = document.createElement("option");
                 NewOptionElement.value = element.options[i].value;
                 var NewTextElement = document.createTextNode(element.options[i].text);
                 NewOptionElement.appendChild(NewTextElement);
 
                 NewSelectElement.appendChild(NewOptionElement);
             }
+
             NewLabelElement.appendChild(NewSelectElement);
             NewTagElement.appendChild(NewLabelElement);
             form.appendChild(NewTagElement);
         }
 
         if(element.type === "radio"){
-            var NewTagElement=document.createElement('p');
-            var NewLabelElement=document.createElement('label');
+            var NewTagElement = document.createElement("p");
+            var NewLabelElement = document.createElement("label");
             var NewTextElement=document.createTextNode(element.label);
             NewLabelElement.appendChild(NewTextElement);
             NewTagElement.appendChild(NewLabelElement);
 
             for( var i = 0; i < element.options.length; i++){
 
-                var NewRadioElement=document.createElement('input');
-                NewRadioElement.value=element.options[i].value;
-                NewRadioElement.type="radio";
-                NewRadioElement.name=element.name;
-                var NewLabelRElement=document.createElement('label');
-                var NewTextElement=document.createTextNode(element.options[i].text);
+                var NewRadioElement = document.createElement("input");
+                NewRadioElement.value = element.options[i].value;
+                NewRadioElement.type = "radio";
+                NewRadioElement.name = element.name;
+                var NewLabelRElement = document.createElement("label");
+                var NewTextElement = document.createTextNode(element.options[i].text);
                 NewLabelRElement.appendChild(NewRadioElement);
                 NewLabelRElement.appendChild(NewTextElement);
                 NewTagElement.appendChild(NewLabelRElement);
-
             }
+
             form.appendChild(NewTagElement);
         }
+
         if(element.type === "checkbox"){
-            var NewTagElement=document.createElement('p');
-            var NewLabelElement=document.createElement('label');
-            var NewTextElement=document.createTextNode(element.label);
+            var NewTagElement = document.createElement("p");
+            var NewLabelElement = document.createElement("label");
+            var NewTextElement = document.createTextNode(element.label);
             NewLabelElement.appendChild(NewTextElement);
 
-            var NewCheckElement=document.createElement('input');
-            NewCheckElement.type="checkbox";
-            NewCheckElement.name=element.name;
-            (element.checked)?NewCheckElement.checked="true":NewCheckElement.checked="false";
+            var NewCheckElement = document.createElement("input");
+            NewCheckElement.type = "checkbox";
+            NewCheckElement.name = element.name;
+            (element.checked) ? NewCheckElement.checked = "true" : NewCheckElement.checked = "false";
             NewLabelElement.appendChild(NewCheckElement);
             NewTagElement.appendChild(NewLabelElement);
             form.appendChild(NewTagElement);
         }
         if(element.type === "textarea"){
-            var NewTagElement=document.createElement('p');
-            var NewLabelElement=document.createElement('label');
-            var NewTextElement=document.createTextNode(element.label);
+            var NewTagElement = document.createElement("p");
+            var NewLabelElement = document.createElement("label");
+            var NewTextElement = document.createTextNode(element.label);
             NewLabelElement.appendChild(NewTextElement);
 
-            var NewTextareaElement=document.createElement('textarea');
-            NewTextareaElement.name=element.name;
-            NewTextareaElement.classList.add(element.class);
+            var NewTextareaElement = document.createElement("textarea");
+            NewTextareaElement.name = element.name;
             NewLabelElement.appendChild(NewTextareaElement);
             NewTagElement.appendChild(NewLabelElement);
             form.appendChild(NewTagElement);
