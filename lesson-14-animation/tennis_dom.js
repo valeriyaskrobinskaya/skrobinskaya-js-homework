395d06542ff3c7b33c8ab8bf503ca91a73089e0d
Valeriya Skrobinskaya <valeriya.skrobinskaya@gmail.com>
2020-05-22 13:28:02 +0300

    Work with elements of tennis in js file

--- lesson-14-animation/tennis_dom.js
+++ lesson-14-animation/tennis_dom.js
@@ -16,3 +16,27 @@ var settimeout; //создаём переменную settimeout для даль
 var messageGoal = document.createElement("div"); //сoздаём div для текста которая будет отображаться когда будет гол
 var messageGoalText = "Гол!";
 
+messageGoal.classList.add("messageGoal");
+messageGoal = wrap.appendChild(messageGoal); // забота с сообщением
+
+wrap.style.width = widthOfWrap;
+wrap.style.height = heightOfWrap; // задаём размеры для wrap
+
+requestAnimationFrame('tick'); // таймер
+
+buttonStart.type = "button"; // работа с кнопкой. Задаём тип
+buttonStart.value = "Старт!"; // текст, который будет отображаться на кнопке
+buttonStart.classList.add("buttonStart"); // присваиваем готовый CSS
+buttonStart = document.body.insertBefore(buttonStart, document.body.children[0]); // делаем кнопку дочерним элементом body
+buttonStart.onclick = start; // на событие onclick вешаем цункцию start
+
+scoreBoard.classList.add("scoreBoard"); // работаем с таблом. Присваиваем готовый CSS.
+scoreBoardInnerHtml(); // вызываем функцию для вывода на табло счёта
+scoreBoard = document.body.insertBefore(scoreBoard, document.body.children[0]); // делаем дочерним элементом body
+
+racquet1.classList.add("racquet1"); // присваиваем готовый CSS для первой ракетки
+racquet2.classList.add("racquet2"); // присваиваем готовый CSS для второй ракетки
+racquet1 = wrap.appendChild(racquet1); //первую(левую) ракетку делаем дочерним элементом wrap
+racquet2 = wrap.appendChild(racquet2); //вторую(правую) ракетку делаем дочерним элементом wrap
+
+
