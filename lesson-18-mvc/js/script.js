bf24b74cd846163b905f1d1e49b4808ee5785a44
Valeriya Skrobinskaya <valeriya.skrobinskaya@gmail.com>
2020-06-02 20:32:39 +0300

    Add var to script.js

---
+++ lesson-18-mvc/js/script.js
@@ -0,0 +1,17 @@
+var drinkStorage = new tLocalStorage("lsDrink"),
+    drinkName = document.getElementById("drinkName"),
+    drinkInfo = document.getElementById("drinkInfo"),
+    drinkInfoDel = document.getElementById("drinkInfoDel"),
+    drinkList = document.getElementById("drinkList"),
+    drinkInfoP = document.getElementById("drinkInfoP");
+
+var dishStorage = new tLocalStorage("lsDish"),
+    dishName = document.getElementById("dishName"),
+    dishInfo = document.getElementById("dishInfo"),
+    dishInfoDel = document.getElementById("dishInfoDel"),
+    dishList = document.getElementById("dishList"),
+    dishInfoP = document.getElementById("dishInfoP");
+
+var store = document.getElementById("store");
+
+
